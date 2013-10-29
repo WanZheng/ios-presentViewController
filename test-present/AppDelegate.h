ce5aa07a19eea79696da5577eecf9eaafb61d66a
Wilson Wan <wanzheng@gmail.com>
2013-10-29 09:57:08 +0800

    Initial Commit

---
+++ test-present/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test-present
+//
+//  Created by WilsonWan on 13-10-29.
+//  Copyright (c) 2013年 WilsonWan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ test-present/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  test-present
+//
+//  Created by WilsonWan on 13-10-29.
+//  Copyright (c) 2013年 WilsonWan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
